e4fb2e9356516445c99a0bbb027aca46b6d50f3a
Mathias231 <mathias.krogh@outlook.com>
2022-07-28 12:09:29 +0200

    Fetching weather data

---
+++ client/src/Data/GetWeather.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function FetchWeatherData() {
+    const [weaterData, setWeaterData] = useState([]);
+    
+    useEffect(() => {
+        axios.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&lon=14.405&altitude=11")
+        .then(res => {
+            setWeaterData(res.data);
+        })
+    }, []);
+
+    if(weaterData) {
+        console.log(weaterData);
+    };
+
+}
+
+
+export default FetchWeatherData;
