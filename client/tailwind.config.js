2b6b8511d4f1c030f84852ff361dd4d24686a5d3
Mathias231 <mathias.krogh@outlook.com>
2022-07-28 12:41:24 +0200

    First commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit', // Enabling just-in-time mode
+  content: ['./src/**/*.{html,js,jsx,t}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
