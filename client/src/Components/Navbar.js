923c821be4088c70e998fc40444efb0a550154a7
Mathias231 <mathias.krogh@outlook.com>
2022-07-28 14:26:03 +0200

    Fixing style

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,8 +5,10 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav class="flex justify-center space-x-4">
-        <Link to="/" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"> Home </Link>
-        <Link to="/Weather" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" > Vær </Link>
+      <div>
+        <Link to="/" class="font-medium px-5 py-2 text-lg text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"> Home </Link>
+        <Link to="/Weather" class="font-medium px-5 py-2 text-lg text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" > Vær </Link>
+      </div>
     </nav>
   )
 }
