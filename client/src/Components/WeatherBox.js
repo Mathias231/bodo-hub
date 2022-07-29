04b3dc25c8cf9a05adaf5788a79b499f0b576d15
Mathias231 <mathias.krogh@outlook.com>
2022-07-29 13:00:30 +0200

    First commit

---
+++ client/src/Components/WeatherBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// API
+import FetchWeatherData from '../Data/GetWeather';
+
+function WeatherBox() {
+  return (
+        <div className="grid grid-cols-3 gap-2 space-x-4 mt-5">
+            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">(!) Været 1 time siden</div>
+            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">04</div>
+            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">04</div>
+            <FetchWeatherData />
+        </div>
+  )
+}
+
+export default WeatherBox
