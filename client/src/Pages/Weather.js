39722e7050f4adb5c94fa4153558dd8d4f1f1117
Mathias231 <mathias.krogh@outlook.com>
2022-07-29 14:54:45 +0200

    Moved code from GetWeather to Weather page (parent)

--- client/src/Pages/Weather.js
+++ client/src/Pages/Weather.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import WeatherBox from '../Components/WeatherBox'
+import WeatherBox from '../Components/WeatherBox';
 
 function Weather() {
+  const [weatherData, setWeaterData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&lon=14.405&altitude=11")
+    .then(res => {
+        setWeaterData(res.data);
+    })
+}, []);
+
+
   return (
     <div className="container mx-auto">
-      <WeatherBox />
+      <WeatherBox weatherData={weatherData} />
     </div>
   )
 }
 
-export default Weather
+export default Weather;
