5184380445dc5943281cf937cfce91a837ebff6d
Mathias231 <mathias.krogh@outlook.com>
2022-08-01 09:29:36 +0200

    Moved api request to Weather main page and sending data to child

--- client/src/Pages/Weather.js
+++ client/src/Pages/Weather.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import WeatherBox from '../Components/WeatherBox';
 
 function Weather() {
+  const url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&lon=14.405&altitude=11";
+  const [weatherData, setWeaterData] = useState(null);
+
+  useEffect(() => {
+    axios.get(url)
+    .then(res => {
+        setWeaterData(res.data);
+    })
+}, [url]);
+
+  if(weatherData) {
+    return (
+      <div className="container mx-auto">
+        <WeatherBox weatherData={weatherData.properties.timeseries}/>
+      </div>
+    )
+  }
 
   return (
     <div className="container mx-auto">
-      <WeatherBox />
+      <h1>Something went wrong</h1>
     </div>
   )
 }
