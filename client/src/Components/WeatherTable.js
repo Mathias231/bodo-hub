557ebf5da9450188cd517c9caacb9dee11035b67
Mathias231 <mathias.krogh@outlook.com>
2022-08-02 12:55:41 +0200

    Tidying

--- client/src/Components/WeatherTable.js
+++ client/src/Components/WeatherTable.js
@@ -3,19 +3,22 @@ import React from 'react'
 function WeatherTable(props) {
   return (
     // div header
-<div className="grid grid-cols-6 gap-4 border-b-4 mt-5">
-  <div className="col-start-2 col-end-4">
-    <div className='grid grid-cols-4'>
-        <h1>Natt</h1>
-        <h1>Morgen</h1>
-        <h1>Ettermiddag</h1>
-        <h1>Kveld</h1>
+    <div className="grid grid-cols-6 gap-4 border-b-4 mt-5">
+        <div className="col-start-2 col-end-4">
+            <div className='grid grid-cols-4'>
+                <h1>Natt</h1>
+                <h1>Morgen</h1>
+                <h1>Ettermiddag</h1>
+                <h1>Kveld</h1>
+            </div>
+        </div>
+        <div className="col-start-4 col-span-2">
+            <div className='grid grid-cols-2'>
+                <h1>Temp</h1>
+                <h1>Vind</h1>
+            </div>
+        </div>
     </div>
-  </div>
-  <div className="col-start-4 col-span-2">
-    test
-  </div>
-</div>
   )
 }
 
