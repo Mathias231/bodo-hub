ddf9a83ca0a75ec30988c6ca7b6dc10c492d264e
Mathias231 <mathias.krogh@outlook.com>
2022-08-02 10:52:06 +0200

    Tidying

--- client/src/Components/WeatherBox.js
+++ client/src/Components/WeatherBox.js
@@ -28,32 +28,32 @@ function WeatherBox(props) {
 
 
   return (
-        <div className="grid grid-cols-3 gap-2 space-x-4 mt-5">
-            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
-              (!) Været for 1 time siden
-              <div className="columns-2">
-                <img src={images[symbolCodeOneHourAgo + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
-                <h1>Temp: {tempOneHourAgo}ºC</h1>
-                <h1>Wind: {windOneHourAgo}m/s</h1>
-              </div>
-            </div>
-            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
-              (!) Været nå
-              <div className="columns-2">
-                <img src={images[symbolCodeNow + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
-                <h1>Temp: {tempNow}ºC</h1>
-                <h1>Wind: {windNow}m/s</h1>
-              </div>
-            </div>
-            <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
-              (!) Været om 1 time
-              <div className="columns-2">
-                <img src={images[symbolCodeNextHour + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
-                <h1>Temp: {tempNextHour}ºC</h1>
-                <h1>Wind: {windNextHour}m/s</h1>
-              </div>
-            </div>
+    <div className="grid grid-cols-3 gap-2 space-x-4 mt-5">
+      <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
+        (!) Været for 1 time siden
+        <div className="columns-2">
+          <img src={images[symbolCodeOneHourAgo + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
+          <h1>Temp: {tempOneHourAgo}ºC</h1>
+          <h1>Wind: {windOneHourAgo}m/s</h1>
         </div>
+      </div>
+      <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
+        (!) Været nå
+        <div className="columns-2">
+          <img src={images[symbolCodeNow + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
+          <h1>Temp: {tempNow}ºC</h1>
+          <h1>Wind: {windNow}m/s</h1>
+        </div>
+      </div>
+      <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
+        (!) Været om 1 time
+        <div className="columns-2">
+          <img src={images[symbolCodeNextHour + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
+          <h1>Temp: {tempNextHour}ºC</h1>
+          <h1>Wind: {windNextHour}m/s</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
