5a78f6aa5f7376519c5770b1168c9bcd1b7c9bcc
Mathias231 <mathias.krogh@outlook.com>
2022-08-02 09:36:56 +0200

    Adding wind and displaying it

--- client/src/Components/WeatherBox.js
+++ client/src/Components/WeatherBox.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function WeatherBox(props) {
+  // Temperature
   const tempOneHourAgo = props.weatherData[1].data.instant.details.air_temperature.toFixed();
   const tempNow = props.weatherData[2].data.instant.details.air_temperature.toFixed();
   const tempNextHour = props.weatherData[3].data.instant.details.air_temperature.toFixed();
+
+  // Wind
+  const windOneHourAgo = props.weatherData[1].data.instant.details.wind_speed;
+  const windNow = props.weatherData[2].data.instant.details.wind_speed;
+  const windNextHour = props.weatherData[3].data.instant.details.wind_speed;
+  
+  // Gets symbol code from weatherData's properties
+  const symbolCodeOneHourAgo = props.weatherData[0].data.next_1_hours.summary.symbol_code; // Shows symbol code 1 hour ago
+  const symbolCodeNow = props.weatherData[1].data.next_1_hours.summary.symbol_code; // Shows current symbol code
+  const symbolCodeNextHour = props.weatherData[2].data.next_1_hours.summary.symbol_code; // Shows symbol code next hour
 
   // A function that maps through images and replaces ./ to empty string | ./clody.svg => cloudy.svg
   function importAll(r) {
@@ -15,10 +26,6 @@ function WeatherBox(props) {
   // Calls function and giving name to array 'images'
   const images = importAll(require.context('../svg', false, /\.(png|jpe?g|svg)$/));
 
-  // Gets symbol code from weatherData's properties
-  const symbolCodeOneHourAgo = props.weatherData[0].data.next_1_hours.summary.symbol_code; // Shows symbol code 1 hour ago
-  const symbolCodeNow = props.weatherData[1].data.next_1_hours.summary.symbol_code; // Shows current symbol code
-  const symbolCodeNextHour = props.weatherData[2].data.next_1_hours.summary.symbol_code; // Shows symbol code next hour
 
   return (
         <div className="grid grid-cols-3 gap-2 space-x-4 mt-5">
@@ -27,7 +34,7 @@ function WeatherBox(props) {
               <div className="columns-2">
                 <img src={images[symbolCodeOneHourAgo + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
                 <h1>Temp: {tempOneHourAgo}ºC</h1>
-                <h1>Wind: </h1>
+                <h1>Wind: {windOneHourAgo}</h1>
               </div>
             </div>
             <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
@@ -35,7 +42,7 @@ function WeatherBox(props) {
               <div className="columns-2">
                 <img src={images[symbolCodeNow + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
                 <h1>Temp: {tempNow}ºC</h1>
-                <h1>Wind: </h1>
+                <h1>Wind: {windNow}</h1>
               </div>
             </div>
             <div className="col-span-1 bg-slate-100 shadow-xl rounded-lg">
@@ -43,7 +50,7 @@ function WeatherBox(props) {
               <div className="columns-2">
                 <img src={images[symbolCodeNextHour + '.svg']} width="100px" className="scale-75 animate-pulse" alt=''></img>
                 <h1>Temp: {tempNextHour}ºC</h1>
-                <h1>Wind: </h1>
+                <h1>Wind: {windNextHour}</h1>
               </div>
             </div>
         </div>
